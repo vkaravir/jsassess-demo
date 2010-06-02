2da18fdf8e39fb909e39b421da1f505d9b43df95
Ville Karavirta <vkaravir@gmail.com>
2010-06-02 22:03:07 +0300

    Unified the usage of dollar and jQuery. Done by wrapping inside a function and jQuery is now passed as a parameter.

--- javascript/jsassess.js
+++ javascript/jsassess.js
@@ -115,19 +115,19 @@ var jsmeterFilters = {
         return html;
     }
 };
-jQuery().ready(function() {
+(function($) { $().ready(function() {
     var handleMessage = function(msg) {
         data = JSON.parse(msg.data);
-        jQuery("#" + data.type + "iframe").remove();
+        $("#" + data.type + "iframe").remove();
         if (data.type === 'jsmeter') {
-            jQuery("#jsassess-" + data.type).html("<h3>Complexity metrics</h3>" + exerciseOptions.jsmeter(data.results));
+            $("#jsassess-" + data.type).html("<h3>Complexity metrics</h3>" + exerciseOptions.jsmeter(data.results));
         } else if (data.type === 'jslint'){
-            jQuery("#jsassess-" + data.type).html("<h3>Programming and style errors</h3>" + data.results);
+            $("#jsassess-" + data.type).html("<h3>Programming and style errors</h3>" + data.results);
         } else if (data.type === 'test') {
-            jQuery("#jsassess-" + data.type).html("<h3>Functionality</h3>" + data.results);
+            $("#jsassess-" + data.type).html("<h3>Functionality</h3>" + data.results);
         }
     }
-    var $jsassessIframes = jQuery("#jsassess-iframes");
+    var $jsassessIframes = $("#jsassess-iframes");
     var run = function(type, exerciseOptions) {
         var jsrunframe = document.createElement("iframe");
         jsrunframe.id = type + "iframe";
@@ -145,17 +145,17 @@ jQuery().ready(function() {
     var elem, elemId, feedbackIds = ["jslint", "test", "jsmeter"];
     for (var i=0; i<feedbackIds.length; i++) {
         elemId = "jsassess-" + feedbackIds[i];
-        if (!jQuery("#" + elemId).length) {
+        if (!$("#" + elemId).length) {
             elem = document.createElement("div");
             elem.id = elemId;
-            jQuery("#jsassess-feedback").append(elem);
+            $("#jsassess-feedback").append(elem);
         }
     }
     if (!$jsassessIframes.length) {
         elem = document.createElement("div");
         elem.id = "jsassess-iframes";
         $jsassessIframes = $(elem);
-        jQuery("body").append(elem);
+        $("body").append(elem);
     }
     var key = document.location.pathname.split('/');
     key = key[key.length-1];
@@ -179,7 +179,7 @@ jQuery().ready(function() {
         syntax: "js",            // syntax to be uses for highgliting
         start_highlight: true        // to display with highlight mode on start-up
     });
-    jQuery("#submitButton").click(function(event) {
+    $("#submitButton").click(function(event) {
         event.preventDefault();
         event.stopPropagation();
 
@@ -205,4 +205,4 @@ jQuery().ready(function() {
     if (window.addEventListener) {
         window.addEventListener( "message", handleMessage, false);
     }
-});
+}) })(jQuery);
